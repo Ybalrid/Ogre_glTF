f0d1f315317e0cc74f6c7a7be2c54bab6383e733
Arthur Brainville <ybalrid@ybalrid.info>
2017-12-25 22:24:01 +0100

    test program initialize ogre (but not much of it) and startup a window

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,13 @@
 
 #include <Ogre_glTF.hpp>
 
+
+#ifdef _DEBUG
+const char RENDER_PLUGIN[] = "RenderSystem_GL3Plus_d";
+#else
+const char RENDER_PLUGIN[] = "RenderSystem_GL3Plus";
+#endif
+
 #ifdef _WIN32
 #include <windows.h>
 INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
@@ -11,8 +18,17 @@ int main(int argc, char* argv[])
 #endif
 {
 	auto root = std::make_unique<Ogre::Root>();
+	root->loadPlugin(RENDER_PLUGIN);
+	root->setRenderSystem(root->getAvailableRenderers().front());
+	root->initialise(false);
 
+	auto window = root->createRenderWindow("glTF test!", 800, 600, false, nullptr);
 	auto gltf = std::make_unique<Ogre_glTF>();
+
+	while(!window->isClosed())
+	{
+		Ogre::WindowEventUtilities::messagePump();
+	}
 
 	return 0;
 }
