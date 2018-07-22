85e2c4d014d61450c3b60ccb1db97bcccc1cf059
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2018-07-23 01:02:46 +0200

    Do log in Debug mode only

--- src/Ogre_glTF_common.cpp
+++ src/Ogre_glTF_common.cpp
@@ -2,6 +2,13 @@
 
 #include <OgreLogManager.h>
 
-void OgreLog(const std::string& message) { Ogre::LogManager::getSingleton().logMessage(message); }
+void OgreLog(const std::string& message)
+{
+#ifdef _DEBUG
+	Ogre::LogManager::getSingleton().logMessage(message);
+#else
+	//Do something with message?
+#endif
+}
 
 void OgreLog(const std::stringstream& message) { OgreLog(message.str()); }
